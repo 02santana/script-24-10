d434aaab78c87ee9f651f971fd2f5fafa987f1af
02santana <rhuanzinhopvp@gmail.com>
2024-11-07 08:43:02 -0300

    a

--- pagina2.js
+++ pagina2.js
@@ -1,9 +1,6 @@
 function criacartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao';
-    cartao.innerHTML = `
-        <div class="conteudo-cartao" >
+
+        <div class="conteudo-cartao" onclick - "mostraResposta" >
        <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -11,7 +8,15 @@ function criacartao(categoria, pergunta, resposta) {
     <div class="resposta-cartao">
         <p>${resposta}</p>
     </div>
-</div >
-    `
+</div>
+    
     container.appendChild(cartao);
 }
+    function mostraResposta (card){
+    const resposta = card.querySelector('.resposta-cartao')
+    if(resposta.style.display === 'none'){
+        resposta.style.display = 'block'
+    }
+    else{
+        resposta.style.display = 'none'
+    }
